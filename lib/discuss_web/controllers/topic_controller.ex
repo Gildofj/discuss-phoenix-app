43204e09b13250d616f010c0bef2efe8b564466e
Gildo Junior <1gildojunior@gmail.com>
2023-05-27 19:43:41 -0300

    improvement: implement edit topic

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -7,17 +7,13 @@ defmodule DiscussWeb.TopicController do
   def index(conn, _params) do
     topics = Repo.all(Topic)
 
-    conn
-    |> put_root_layout(html: false)
-    |> render(:index, topics: topics)
+    renderPage conn, :index, topics: topics
   end
 
   def new(conn, _params) do
    changeset = Topic.changeset(%Topic{}, %{})
 
-   conn
-   |> put_root_layout(html: false)
-   |> render(:new, changeset: changeset)
+   renderPage conn, :new, changeset: changeset
   end
 
   def create(conn, %{"topic" => topic}) do
@@ -28,10 +24,45 @@ defmodule DiscussWeb.TopicController do
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: path(conn, ~p"/"))
-      {:error, changeset} ->
-        conn
-        |> put_root_layout(html: false)
-        |> render(:new, changeset: changeset)
+      {:error, changeset} -> renderPage conn, :new, changeset: changeset
     end
   end
+
+  def edit(conn, %{"id" => id}) do
+    topic = Repo.get(Topic, id)
+    changeset = Topic.changeset(topic)
+
+    renderPage conn, :edit, changeset: changeset, id: id
+  end
+
+  def update(conn, %{"id" => id, "topic" => topic}) do
+    changeset = Repo.get(Topic, id) |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: path(conn, ~p"/"))
+      {:error, changeset} -> renderPage conn, :edit, changeset: changeset, id: id
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    changeset = Repo.get(Topic, id)
+
+    case Repo.delete(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Removed")
+        |> renderPage(:index)
+      {:error, changeset} -> renderPage conn, :index, changeset
+    end
+  end
+
+  defp renderPage(conn, path, args \\ %{}) do
+    conn
+    |> put_root_layout(html: false)
+    |> render(path, args)
+  end
+
 end
